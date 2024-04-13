a0985df65e787ddfea2117164d3be59a415feae6
lepidodendronnnn <alexandrevnas@gmail.com>
2024-04-13 19:41:24 +0300

    meow

--- src/main/java/commands/ExecuteScriptCommand.java
+++ src/main/java/commands/ExecuteScriptCommand.java
@@ -5,7 +5,6 @@ import managers.*;
 public class ExecuteScriptCommand extends Command{
 
     private final ConsoleManager console;
-    int x = 0;
 
     public ExecuteScriptCommand(ConsoleManager console) {
         super("execute_script file_name", "считать и исполнить скрипт из указанного файла (в скрипте содержатся команды в таком же виде, в котором их вводит пользователь в интерактивном режиме)");
@@ -17,14 +16,9 @@ public class ExecuteScriptCommand extends Command{
         try {
             if (argument.isEmpty()) throw new IllegalArgumentException();
             console.println("Выполнение скрипта '" + argument + "'...");
-            x++;
             return true;
         } catch (IllegalArgumentException exception) {
             console.printError("Нет аргументов для исполнения команды '" + getName() + "'");
-        }
-        if (x > 20){
-            x = 0;
-            System.exit(1);
         }
         return false;
     }
